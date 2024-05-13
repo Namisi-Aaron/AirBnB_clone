7aac88c053812856f93fd275cfb7ce30d9f7ce08
Namisi-Aaron <aaronnamisi@gmail.com>
2024-05-13 06:01:11 -0700

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+"""This module contains the definition of the BaseModel class"""
+
+
+class BaseModel:
+    """Defines the class BaseModel"""
+    def __init__(self):
+        """Is the initialization method for the BaseModel class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Prints [<class name>] (<self.id>) <self.__dict__>"""
+        return '[{}] ({}) ({})'.format(self.__class__.__name__,
+                                       self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
