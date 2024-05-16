2438079ef609a7a45bed361b8bec497f139833ce
Namisi-Aaron <aaronnamisi@gmail.com>
2024-05-15 23:26:56 -0700

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+"""This is the  console module"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """This is the class definition for
+    HBNBCommand, inheriting from cmd.Cmd"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command exits the program"""
+
+        if arg.lower() == 'quit':
+            return True
+        elif arg == '':
+            return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
